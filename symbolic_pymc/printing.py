brandonwillard/symbolic-pymc3

Initial PyMC3-to-Theano model conversion functionality

spymc3/printing.py
@@ -8,6 +8,8 @@ import theano.tensor as tt
 from copy import copy
 from collections import OrderedDict
 
+from theano import gof
+
 from sympy import Array as SympyArray
 from sympy.printing import latex as sympy_latex
 
@@ -17,9 +19,10 @@ from .rv import RandomVariable
 
 
 # [[file:~/projects/websites/brandonwillard.github.io/content/articles/src/org/symbolic-math-in-pymc3-new-op.org::rv-pprinter-imp][rv-pprinter-imp]]
-class RandomVariablePrinter:
+class RandomVariablePrinter(object):
     """Pretty print random variables.
     """
+
     def __init__(self, name=None):
         """
         Parameters
@@ -94,7 +97,7 @@ class RandomVariablePrinter:
 
 
 # [[file:~/projects/websites/brandonwillard.github.io/content/articles/src/org/symbolic-math-in-pymc3-new-op.org::variable-tag-printer-class][variable-tag-printer-class]]
-class VariableWithShapePrinter:
+class VariableWithShapePrinter(object):
     """Print variable shape info in the preamble and use readable character
     names for unamed variables.
     """
@@ -108,7 +111,7 @@ class VariableWithShapePrinter:
 
         using_latex = getattr(pstate, 'latex', False)
 
-        if isinstance(output, tt.gof.Constant):
+        if isinstance(output, gof.Constant):
             if output.ndim > 0 and using_latex:
                 out_name = sympy_latex(SympyArray(output.data))
             else:
@@ -127,15 +130,14 @@ class VariableWithShapePrinter:
         return out_name
 
     @classmethod
-    def process_shape_name(cls, output, pstate):
-        shape_of_var = output.owner.inputs[0]
-        shape_names = pstate.memo.setdefault('shape_names', {})
-        out_name = shape_names.setdefault(
-            shape_of_var, cls.process_variable_name(output, pstate))
-        return out_name
-
-    @classmethod
     def process_variable_name(cls, output, pstate):
+        """Take a variable name from the available ones.
+
+        This function also initializes the available names by
+        removing all the manually specified names within the
+        `FunctionGraph` being printed (if available).
+        Doing so removes the potential for name collisions.
+        """
         if output in pstate.memo:
             return pstate.memo[output]
 
@@ -233,13 +235,22 @@ class VariableWithShapePrinter:
 class PreamblePPrinter(theano.printing.PPrinter):
     """Pretty printer that displays a preamble.
 
-    For example,
+    Example
+    =======
 
-        X ~ N(\\mu, \\sigma)
-        (b * X)
+    >>> import theano.tensor as tt
+    >>> from spymc3 import NormalRV
+    >>> from spymc3.printing import tt_pprint
+    >>> X_rv = NormalRV(tt.scalar('\\mu'), tt.scalar('\\sigma'), name='X')
+    >>> print(tt_pprint(X_rv))
+    \mu in R
+    \sigma in R
+    X ~ N(\mu, \sigma**2),  X in R
+    X
 
     XXX: Not thread-safe!
     """
+
     def __init__(self, *args, pstate_defaults=None, **kwargs):
         """
         Parameters
@@ -281,7 +292,7 @@ class PreamblePPrinter(theano.printing.PPrinter):
 
     def process_graph(self, inputs, outputs, updates=None,
                       display_inputs=False):
-        raise NotImplemented()
+        raise NotImplementedError()
 
     def __call__(self, *args, latex_env='equation', latex_label=None):
         var = args[0]
@@ -290,33 +301,43 @@ class PreamblePPrinter(theano.printing.PPrinter):
             pstate = self.create_state(args[1])
         elif pstate is None:
             pstate = self.create_state(None)
-        # else:
-        #     # XXX: The graph processing doesn't pass around the printer state!
-        #     # TODO: We'll have to copy the code and fix it...
-        #     raise NotImplemented('No preambles for graph printing, yet.')
 
         # This pretty printer needs more information about shapes and inputs,
         # which it gets from a `FunctionGraph`.  Create one, if `var` isn't
         # already assigned one.
-        fgraph = getattr(var, 'fgraph', None)
+        if isinstance(var, gof.fg.FunctionGraph):
+            fgraph = var
+            if not hasattr(fgraph, 'shape_feature'):
+                shape_feature = tt.opt.ShapeFeature()
+                fgraph.attach_feature(shape_feature)
+            out_vars = fgraph.outputs
+        else:
+            fgraph = getattr(var, 'fgraph', None)
+            out_vars = [var]
+
         if not fgraph:
-            fgraph = tt.gof.fg.FunctionGraph(
-                tt.gof.graph.inputs([var]), [var])
-            var = fgraph.outputs[0]
+            fgraph = gof.fg.FunctionGraph(
+                gof.graph.inputs([var]), [var])
+            out_vars = fgraph.outputs
 
             # Use this to get better shape info
             shape_feature = tt.opt.ShapeFeature()
             fgraph.attach_feature(shape_feature)
 
-        body_str = super().__call__(var, pstate)
+        # TODO: How should this be formatted to better designate
+        # the output numbers (in LaTeX, as well)?
+        body_strs = []
+        for v in out_vars:
+            body_strs += [super().__call__(v, pstate)]
 
         latex_out = getattr(pstate, 'latex', False)
         if pstate.preamble_lines and latex_out:
-            preamble_str = "\n\\\\\n".join(pstate.preamble_lines)
-            preamble_str = "\\begin{gathered}\n%s\n\\end{gathered}" % (preamble_str)
-            res = "\n\\\\\n".join([preamble_str, body_str])
+            preamble_body = "\n\\\\\n".join(pstate.preamble_lines)
+            preamble_str = "\\begin{gathered}\n%s\n\\end{gathered}"
+            preamble_str = preamble_str % (preamble_body)
+            res = "\n\\\\\n".join([preamble_str] + body_strs)
         else:
-            res = "\n".join(pstate.preamble_lines + [body_str])
+            res = "\n".join(pstate.preamble_lines + body_strs)
 
         if latex_out and latex_env:
             label_out = f'\\label{{{latex_label}}}\n' if latex_label else ''
@@ -339,6 +360,40 @@ tt_pprint.assign(GammaRV, RandomVariablePrinter('Gamma'))
 tt_pprint.assign(ExponentialRV, RandomVariablePrinter('Exp'))
 
 
+class ObservationPrinter(object):
+
+    def process(self, output, pstate):
+        if output in pstate.memo:
+            return pstate.memo[output]
+
+        pprinter = pstate.pprinter
+        node = output.owner
+
+        if node is None or not isinstance(node.op, Observed):
+            raise TypeError(f'Node Op is not of type `Observed`: {node.op}')
+
+        val = node.inputs[0]
+        rv = node.inputs[1]
+        new_precedence = -1000
+        try:
+            old_precedence = getattr(pstate, 'precedence', None)
+            pstate.precedence = new_precedence
+
+            val_name = pprinter.process(val, pstate)
+
+            rv_name = pprinter.process(rv, pstate)
+
+            out_name = f'{rv_name} = {val_name}'
+        finally:
+            pstate.precedence = old_precedence
+
+        pstate.memo[output] = out_name
+        return out_name
+
+
+tt_pprint.assign(Observed, ObservationPrinter())
+
+
 class NormalRVPrinter(RandomVariablePrinter):
     def __init__(self):
         super().__init__('N')
@@ -354,7 +409,7 @@ class NormalRVPrinter(RandomVariablePrinter):
 
 
 tt_pprint.assign(NormalRV, NormalRVPrinter())
-tt_pprint.assign(MvNormalRV, NormalRVPrinter())
+tt_pprint.assign(MvNormalRV, RandomVariablePrinter('N'))
 
 tt_pprint.assign(DirichletRV, RandomVariablePrinter('Dir'))
 tt_pprint.assign(PoissonRV, RandomVariablePrinter('Pois'))

tests/test_kanren.py
@@ -6,7 +6,7 @@ from kanren.term import term, operator, arguments
 from kanren.assoccomm import eq_assoc, eq_comm
 from unification import var
 
-from spymc3.utils import graph_equal, to_meta
+from spymc3.utils import graph_equal
 from spymc3 import MvNormalRV
 from spymc3.meta import mt
 # theano-kanren-imports ends here
@@ -17,8 +17,8 @@ def test_terms():
     x, a, b = tt.dvectors('xab')
     test_expr = x + a * b
 
-    assert to_meta(test_expr.owner.op) == operator(test_expr)
-    assert to_meta(tuple(test_expr.owner.inputs)) == arguments(test_expr)
+    assert mt(test_expr.owner.op) == operator(test_expr)
+    assert mt(tuple(test_expr.owner.inputs)) == arguments(test_expr)
     assert graph_equal(test_expr, term(operator(test_expr),
                                        arguments(test_expr)))
 # theano-term-tests ends here
@@ -84,10 +84,10 @@ def test_assoccomm():
     assert graph_equal(tuple(test_expr.owner.inputs),
                        run(1, q, buildo(tt.add, q, test_expr))[0])
 
-    assert (to_meta(a),) == run(0, var('x'),
-                                (eq_comm, mt.mul(a, b), mt.mul(b, var('x'))))
-    assert (to_meta(a),) == run(0, var('x'),
-                                (eq_comm, mt.add(a, b), mt.add(b, var('x'))))
+    assert (mt(a),) == run(0, var('x'),
+                           (eq_comm, mt.mul(a, b), mt.mul(b, var('x'))))
+    assert (mt(a),) == run(0, var('x'),
+                           (eq_comm, mt.add(a, b), mt.add(b, var('x'))))
 
     res = run(0, var('x'),
               (eq_assoc,

tests/test_pymc3.py
@@ -1,0 +1,108 @@
+import pytest
+
+import numpy as np
+import theano
+import theano.tensor as tt
+import pymc3 as pm
+
+from theano.configparser import change_flags
+from theano.gof import FunctionGraph, Feature, NodeFinder
+from theano.gof.graph import inputs as tt_inputs, clone_get_equiv
+
+from spymc3 import *
+from spymc3.pymc3 import *
+from spymc3.utils import graph_equal, canonicalize
+from spymc3.meta import mt
+
+tt.config.on_opt_error = 'raise'
+tt.config.compute_test_value = 'ignore'
+theano.config.mode = 'FAST_COMPILE'
+theano.config.cxx = ''
+
+
+# @change_flags
+def test_normals():
+    mu_X = tt.scalar('mu_X')
+    sd_X = tt.scalar('sd_X')
+    mu_Y = tt.scalar('mu_Y')
+    sd_Y = tt.scalar('sd_Y')
+    mu_X.tag.test_value = np.array(0., dtype=tt.config.floatX)
+    sd_X.tag.test_value = np.array(1., dtype=tt.config.floatX)
+    mu_Y.tag.test_value = np.array(1., dtype=tt.config.floatX)
+    sd_Y.tag.test_value = np.array(0.5, dtype=tt.config.floatX)
+
+    with pm.Model() as model:
+        X_rv = pm.Normal('X_rv', mu_X, sd=sd_X)
+        Y_rv = pm.Normal('Y_rv', mu_Y, sd=sd_Y)
+        Z_rv = pm.Normal('Z_rv',
+                         X_rv + Y_rv,
+                         sd=sd_X + sd_Y,
+                         observed=10.)
+
+    fgraph = model_graph(model, output_vars=[Z_rv])
+    Z_rv_tt = canonicalize(fgraph)
+
+    # This will break comparison if we don't reuse it
+    rng = Z_rv_tt.owner.inputs[1].owner.inputs[-1]
+
+    mu_X_ = mt.scalar('mu_X')
+    sd_X_ = mt.scalar('sd_X')
+    mu_Y_ = mt.scalar('mu_Y')
+    sd_Y_ = mt.scalar('sd_Y')
+    tt.config.compute_test_value = 'ignore'
+    X_rv_ = mt.NormalRV(mu_X_, sd_X_, None, rng, name='X_rv')
+    Y_rv_ = mt.NormalRV(mu_Y_, sd_Y_, None, rng, name='Y_rv')
+    Z_rv_ = mt.NormalRV(mt.add(X_rv_, Y_rv_),
+                        mt.add(sd_X_, sd_Y_),
+                        None, rng, name='Z_rv')
+    Z_rv_ = mt.observed(Z_rv.observations, Z_rv_)
+
+    Z_rv_meta = canonicalize(Z_rv_.reify())
+
+    assert mt(Z_rv_tt) == mt(Z_rv_meta)
+
+
+def test_broadcastable():
+    mu_X = tt.vector('mu_X')
+    sd_X = tt.vector('sd_X')
+    mu_Y = tt.vector('mu_Y')
+    sd_Y = tt.vector('sd_Y')
+    mu_X.tag.test_value = np.array([0.], dtype=tt.config.floatX)
+    sd_X.tag.test_value = np.array([1.], dtype=tt.config.floatX)
+    mu_Y.tag.test_value = np.array([1.], dtype=tt.config.floatX)
+    sd_Y.tag.test_value = np.array([0.5], dtype=tt.config.floatX)
+
+    with pm.Model() as model:
+        X_rv = pm.Normal('X_rv', mu_X, sd=sd_X, shape=(1,))
+        Y_rv = pm.Normal('Y_rv', mu_Y, sd=sd_Y, shape=(1,))
+        Z_rv = pm.Normal('Z_rv',
+                         X_rv + Y_rv,
+                         sd=sd_X + sd_Y,
+                         shape=(1,),
+                         observed=[10.])
+
+    with pytest.warns(UserWarning):
+        fgraph = model_graph(model)
+
+    Z_rv_tt = canonicalize(fgraph)
+
+    # This will break comparison if we don't reuse it
+    rng = Z_rv_tt.owner.inputs[1].owner.inputs[-1]
+
+    mu_X_ = mt.vector('mu_X')
+    sd_X_ = mt.vector('sd_X')
+    mu_Y_ = mt.vector('mu_Y')
+    sd_Y_ = mt.vector('sd_Y')
+    tt.config.compute_test_value = 'ignore'
+    X_rv_ = mt.NormalRV(mu_X_, sd_X_, None, rng, name='X_rv')
+    X_rv_ = mt.addbroadcast(X_rv_, 0)
+    Y_rv_ = mt.NormalRV(mu_Y_, sd_Y_, None, rng, name='Y_rv')
+    Y_rv_ = mt.addbroadcast(Y_rv_, 0)
+    Z_rv_ = mt.NormalRV(mt.add(X_rv_, Y_rv_),
+                        mt.add(sd_X_, sd_Y_),
+                        None, rng, name='Z_rv')
+    Z_rv_ = mt.observed(Z_rv.observations, Z_rv_)
+
+    Z_rv_meta = canonicalize(Z_rv_.reify())
+
+    assert mt(Z_rv_tt) == mt(Z_rv_meta)

pymc-devs/symbolic-pymc

symbolic_pymc/printing.py
@@ -7,6 +7,8 @@ import theano.tensor as tt
 from copy import copy
 from collections import OrderedDict
 
+from theano import gof
+
 from sympy import Array as SympyArray
 from sympy.printing import latex as sympy_latex
 
@@ -14,7 +16,7 @@ from . import *
 from .rv import RandomVariable
 
 
-class RandomVariablePrinter:
+class RandomVariablePrinter(object):
     """Pretty print random variables.
     """
 
@@ -90,7 +92,7 @@ class RandomVariablePrinter:
         return out_name
 
 
-class VariableWithShapePrinter:
+class VariableWithShapePrinter(object):
     """Print variable shape info in the preamble and use readable character
     names for unamed variables.
     """
@@ -104,7 +106,7 @@ class VariableWithShapePrinter:
 
         using_latex = getattr(pstate, 'latex', False)
 
-        if isinstance(output, tt.gof.Constant):
+        if isinstance(output, gof.Constant):
             if output.ndim > 0 and using_latex:
                 out_name = sympy_latex(SympyArray(output.data))
             else:
@@ -123,15 +125,14 @@ class VariableWithShapePrinter:
         return out_name
 
     @classmethod
-    def process_shape_name(cls, output, pstate):
-        shape_of_var = output.owner.inputs[0]
-        shape_names = pstate.memo.setdefault('shape_names', {})
-        out_name = shape_names.setdefault(
-            shape_of_var, cls.process_variable_name(output, pstate))
-        return out_name
-
-    @classmethod
     def process_variable_name(cls, output, pstate):
+        """Take a variable name from the available ones.
+
+        This function also initializes the available names by
+        removing all the manually specified names within the
+        `FunctionGraph` being printed (if available).
+        Doing so removes the potential for name collisions.
+        """
         if output in pstate.memo:
             return pstate.memo[output]
 
@@ -227,13 +228,22 @@ class VariableWithShapePrinter:
 class PreamblePPrinter(theano.printing.PPrinter):
     """Pretty printer that displays a preamble.
 
-    For example,
+    Example
+    =======
 
-        X ~ N(\\mu, \\sigma)
-        (b * X)
+    >>> import theano.tensor as tt
+    >>> from symbolic_pymc import NormalRV
+    >>> from symbolic_pymc.printing import tt_pprint
+    >>> X_rv = NormalRV(tt.scalar('\\mu'), tt.scalar('\\sigma'), name='X')
+    >>> print(tt_pprint(X_rv))
+    \\mu in R
+    \\sigma in R
+    X ~ N(\\mu, \\sigma**2),  X in R
+    X
 
     XXX: Not thread-safe!
     """
+
     def __init__(self, *args, pstate_defaults=None, **kwargs):
         """
         Parameters
@@ -275,7 +285,7 @@ class PreamblePPrinter(theano.printing.PPrinter):
 
     def process_graph(self, inputs, outputs, updates=None,
                       display_inputs=False):
-        raise NotImplemented()
+        raise NotImplementedError()
 
     def __call__(self, *args, latex_env='equation', latex_label=None):
         var = args[0]
@@ -284,33 +294,43 @@ class PreamblePPrinter(theano.printing.PPrinter):
             pstate = self.create_state(args[1])
         elif pstate is None:
             pstate = self.create_state(None)
-        # else:
-        #     # XXX: The graph processing doesn't pass around the printer state!
-        #     # TODO: We'll have to copy the code and fix it...
-        #     raise NotImplemented('No preambles for graph printing, yet.')
 
         # This pretty printer needs more information about shapes and inputs,
         # which it gets from a `FunctionGraph`.  Create one, if `var` isn't
         # already assigned one.
-        fgraph = getattr(var, 'fgraph', None)
+        if isinstance(var, gof.fg.FunctionGraph):
+            fgraph = var
+            if not hasattr(fgraph, 'shape_feature'):
+                shape_feature = tt.opt.ShapeFeature()
+                fgraph.attach_feature(shape_feature)
+            out_vars = fgraph.outputs
+        else:
+            fgraph = getattr(var, 'fgraph', None)
+            out_vars = [var]
+
         if not fgraph:
-            fgraph = tt.gof.fg.FunctionGraph(
-                tt.gof.graph.inputs([var]), [var])
-            var = fgraph.outputs[0]
+            fgraph = gof.fg.FunctionGraph(
+                gof.graph.inputs([var]), [var])
+            out_vars = fgraph.outputs
 
             # Use this to get better shape info
             shape_feature = tt.opt.ShapeFeature()
             fgraph.attach_feature(shape_feature)
 
-        body_str = super().__call__(var, pstate)
+        # TODO: How should this be formatted to better designate
+        # the output numbers (in LaTeX, as well)?
+        body_strs = []
+        for v in out_vars:
+            body_strs += [super().__call__(v, pstate)]
 
         latex_out = getattr(pstate, 'latex', False)
         if pstate.preamble_lines and latex_out:
-            preamble_str = "\n\\\\\n".join(pstate.preamble_lines)
-            preamble_str = "\\begin{gathered}\n%s\n\\end{gathered}" % (preamble_str)
-            res = "\n\\\\\n".join([preamble_str, body_str])
+            preamble_body = "\n\\\\\n".join(pstate.preamble_lines)
+            preamble_str = "\\begin{gathered}\n%s\n\\end{gathered}"
+            preamble_str = preamble_str % (preamble_body)
+            res = "\n\\\\\n".join([preamble_str] + body_strs)
         else:
-            res = "\n".join(pstate.preamble_lines + [body_str])
+            res = "\n".join(pstate.preamble_lines + body_strs)
 
         if latex_out and latex_env:
             label_out = f'\\label{{{latex_label}}}\n' if latex_label else ''
@@ -331,6 +351,40 @@ tt_pprint.assign(GammaRV, RandomVariablePrinter('Gamma'))
 tt_pprint.assign(ExponentialRV, RandomVariablePrinter('Exp'))
 
 
+class ObservationPrinter(object):
+
+    def process(self, output, pstate):
+        if output in pstate.memo:
+            return pstate.memo[output]
+
+        pprinter = pstate.pprinter
+        node = output.owner
+
+        if node is None or not isinstance(node.op, Observed):
+            raise TypeError(f'Node Op is not of type `Observed`: {node.op}')
+
+        val = node.inputs[0]
+        rv = node.inputs[1]
+        new_precedence = -1000
+        try:
+            old_precedence = getattr(pstate, 'precedence', None)
+            pstate.precedence = new_precedence
+
+            val_name = pprinter.process(val, pstate)
+
+            rv_name = pprinter.process(rv, pstate)
+
+            out_name = f'{rv_name} = {val_name}'
+        finally:
+            pstate.precedence = old_precedence
+
+        pstate.memo[output] = out_name
+        return out_name
+
+
+tt_pprint.assign(Observed, ObservationPrinter())
+
+
 class NormalRVPrinter(RandomVariablePrinter):
     def __init__(self):
         super().__init__('N')
@@ -346,7 +400,7 @@ class NormalRVPrinter(RandomVariablePrinter):
 
 
 tt_pprint.assign(NormalRV, NormalRVPrinter())
-tt_pprint.assign(MvNormalRV, NormalRVPrinter())
+tt_pprint.assign(MvNormalRV, RandomVariablePrinter('N'))
 
 tt_pprint.assign(DirichletRV, RandomVariablePrinter('Dir'))
 tt_pprint.assign(PoissonRV, RandomVariablePrinter('Pois'))

tests/test_kanren.py
@@ -5,7 +5,7 @@ from kanren.term import term, operator, arguments
 from kanren.assoccomm import eq_assoc, eq_comm
 from unification import var
 
-from symbolic_pymc.utils import graph_equal, to_meta
+from symbolic_pymc.utils import graph_equal
 from symbolic_pymc import MvNormalRV
 from symbolic_pymc.meta import mt
 
@@ -14,8 +14,8 @@ def test_terms():
     x, a, b = tt.dvectors('xab')
     test_expr = x + a * b
 
-    assert to_meta(test_expr.owner.op) == operator(test_expr)
-    assert to_meta(tuple(test_expr.owner.inputs)) == arguments(test_expr)
+    assert mt(test_expr.owner.op) == operator(test_expr)
+    assert mt(tuple(test_expr.owner.inputs)) == arguments(test_expr)
     assert graph_equal(test_expr, term(operator(test_expr),
                                        arguments(test_expr)))
 
@@ -76,10 +76,10 @@ def test_assoccomm():
     assert graph_equal(tuple(test_expr.owner.inputs),
                        run(1, q, buildo(tt.add, q, test_expr))[0])
 
-    assert (to_meta(a),) == run(0, var('x'),
-                                (eq_comm, mt.mul(a, b), mt.mul(b, var('x'))))
-    assert (to_meta(a),) == run(0, var('x'),
-                                (eq_comm, mt.add(a, b), mt.add(b, var('x'))))
+    assert (mt(a),) == run(0, var('x'),
+                           (eq_comm, mt.mul(a, b), mt.mul(b, var('x'))))
+    assert (mt(a),) == run(0, var('x'),
+                           (eq_comm, mt.add(a, b), mt.add(b, var('x'))))
 
     res = run(0, var('x'),
               (eq_assoc,

tests/test_pymc3.py
@@ -1,0 +1,108 @@
+import pytest
+
+import numpy as np
+import theano
+import theano.tensor as tt
+import pymc3 as pm
+
+from theano.configparser import change_flags
+from theano.gof import FunctionGraph, Feature, NodeFinder
+from theano.gof.graph import inputs as tt_inputs, clone_get_equiv
+
+from symbolic_pymc import *
+from symbolic_pymc.pymc3 import *
+from symbolic_pymc.utils import graph_equal, canonicalize
+from symbolic_pymc.meta import mt
+
+tt.config.on_opt_error = 'raise'
+tt.config.compute_test_value = 'ignore'
+theano.config.mode = 'FAST_COMPILE'
+theano.config.cxx = ''
+
+
+# @change_flags
+def test_normals():
+    mu_X = tt.scalar('mu_X')
+    sd_X = tt.scalar('sd_X')
+    mu_Y = tt.scalar('mu_Y')
+    sd_Y = tt.scalar('sd_Y')
+    mu_X.tag.test_value = np.array(0., dtype=tt.config.floatX)
+    sd_X.tag.test_value = np.array(1., dtype=tt.config.floatX)
+    mu_Y.tag.test_value = np.array(1., dtype=tt.config.floatX)
+    sd_Y.tag.test_value = np.array(0.5, dtype=tt.config.floatX)
+
+    with pm.Model() as model:
+        X_rv = pm.Normal('X_rv', mu_X, sd=sd_X)
+        Y_rv = pm.Normal('Y_rv', mu_Y, sd=sd_Y)
+        Z_rv = pm.Normal('Z_rv',
+                         X_rv + Y_rv,
+                         sd=sd_X + sd_Y,
+                         observed=10.)
+
+    fgraph = model_graph(model, output_vars=[Z_rv])
+    Z_rv_tt = canonicalize(fgraph)
+
+    # This will break comparison if we don't reuse it
+    rng = Z_rv_tt.owner.inputs[1].owner.inputs[-1]
+
+    mu_X_ = mt.scalar('mu_X')
+    sd_X_ = mt.scalar('sd_X')
+    mu_Y_ = mt.scalar('mu_Y')
+    sd_Y_ = mt.scalar('sd_Y')
+    tt.config.compute_test_value = 'ignore'
+    X_rv_ = mt.NormalRV(mu_X_, sd_X_, None, rng, name='X_rv')
+    Y_rv_ = mt.NormalRV(mu_Y_, sd_Y_, None, rng, name='Y_rv')
+    Z_rv_ = mt.NormalRV(mt.add(X_rv_, Y_rv_),
+                        mt.add(sd_X_, sd_Y_),
+                        None, rng, name='Z_rv')
+    Z_rv_ = mt.observed(Z_rv.observations, Z_rv_)
+
+    Z_rv_meta = canonicalize(Z_rv_.reify())
+
+    assert mt(Z_rv_tt) == mt(Z_rv_meta)
+
+
+def test_broadcastable():
+    mu_X = tt.vector('mu_X')
+    sd_X = tt.vector('sd_X')
+    mu_Y = tt.vector('mu_Y')
+    sd_Y = tt.vector('sd_Y')
+    mu_X.tag.test_value = np.array([0.], dtype=tt.config.floatX)
+    sd_X.tag.test_value = np.array([1.], dtype=tt.config.floatX)
+    mu_Y.tag.test_value = np.array([1.], dtype=tt.config.floatX)
+    sd_Y.tag.test_value = np.array([0.5], dtype=tt.config.floatX)
+
+    with pm.Model() as model:
+        X_rv = pm.Normal('X_rv', mu_X, sd=sd_X, shape=(1,))
+        Y_rv = pm.Normal('Y_rv', mu_Y, sd=sd_Y, shape=(1,))
+        Z_rv = pm.Normal('Z_rv',
+                         X_rv + Y_rv,
+                         sd=sd_X + sd_Y,
+                         shape=(1,),
+                         observed=[10.])
+
+    with pytest.warns(UserWarning):
+        fgraph = model_graph(model)
+
+    Z_rv_tt = canonicalize(fgraph)
+
+    # This will break comparison if we don't reuse it
+    rng = Z_rv_tt.owner.inputs[1].owner.inputs[-1]
+
+    mu_X_ = mt.vector('mu_X')
+    sd_X_ = mt.vector('sd_X')
+    mu_Y_ = mt.vector('mu_Y')
+    sd_Y_ = mt.vector('sd_Y')
+    tt.config.compute_test_value = 'ignore'
+    X_rv_ = mt.NormalRV(mu_X_, sd_X_, None, rng, name='X_rv')
+    X_rv_ = mt.addbroadcast(X_rv_, 0)
+    Y_rv_ = mt.NormalRV(mu_Y_, sd_Y_, None, rng, name='Y_rv')
+    Y_rv_ = mt.addbroadcast(Y_rv_, 0)
+    Z_rv_ = mt.NormalRV(mt.add(X_rv_, Y_rv_),
+                        mt.add(sd_X_, sd_Y_),
+                        None, rng, name='Z_rv')
+    Z_rv_ = mt.observed(Z_rv.observations, Z_rv_)
+
+    Z_rv_meta = canonicalize(Z_rv_.reify())
+
+    assert mt(Z_rv_tt) == mt(Z_rv_meta)