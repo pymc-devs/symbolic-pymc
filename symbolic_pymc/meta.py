0781f07a3e493321f05e768f21fae6dd1dc4f64a
Brandon T. Willard <brandonwillard@gmail.com>
2019-02-16 00:18:02 -0600

    Make meta conversion handle types; better reification and `Op.__call__`.

--- symbolic_pymc/meta.py
+++ symbolic_pymc/meta.py
@@ -7,10 +7,13 @@ import numpy as np
 import theano
 import theano.tensor as tt
 
-from functools import partial, wraps
 from itertools import chain
+from functools import partial, wraps
+from collections.abc import Iterator
 
 from unification import var, isvar, Var
+
+from .rv import RandomVariable
 
 # TODO: Replace `from_obj` with a dispatched function?
 # from multipledispatch import dispatch
@@ -29,6 +32,10 @@ def _meta_reify_iter(rands):
         elif MetaSymbol.is_meta(s):
             reified_rands += [s]
             any_unreified |= True
+        elif isinstance(s, (list, tuple)):
+            _reified_rands, _any_unreified = _meta_reify_iter(s)
+            reified_rands += [type(s)(_reified_rands)]
+            any_unreified |= _any_unreified
         else:
             reified_rands += [s]
 
@@ -106,12 +113,28 @@ class MetaSymbol(metaclass=MetaSymbolType):
         XXX: Be careful when overriding this: `isvar` checks are necessary!
         """
         if (cls.is_meta(obj) or obj is None or
-                isinstance(obj, (types.FunctionType, partial))):
+                isinstance(obj, (types.FunctionType, partial,
+                                 str, dict))):
             return obj
 
-        if isinstance(obj, (list, tuple)):
+        if isinstance(obj, (set, list, tuple, Iterator)):
             # Convert elements of the iterable
             return type(obj)([cls.from_obj(o) for o in obj])
+
+        if inspect.isclass(obj) and issubclass(obj, cls.base_classes()):
+            # This is a class/type covered by a meta class/type.
+            try:
+                obj_cls = next(filter(lambda t: issubclass(obj, t.base),
+                                      cls.__subclasses__()))
+            except StopIteration:
+                # The current class is the best fit.
+                if cls.base == obj:
+                    return cls.base
+                # This object is a subclass of the base type.
+                new_type = type(f'Meta{obj.__name__}', (cls,), {})
+                return new_type(obj)
+            else:
+                return obj_cls.from_obj(obj)
 
         if not isinstance(obj, cls.base_classes()):
             # We might've been given something convertible to a type with a
@@ -265,40 +288,104 @@ class MetaTensorType(MetaType):
 
 
 class MetaOp(MetaSymbol):
+    """A meta object that represents Theano `Op`s.
+
+    NOTE: By default it will use `Op.make_node`'s signature to produce meta
+    `Apply` node inputs, so be sure to override that signature when
+    `Op.make_node`'s arguments aren't one-to-one with the expected `Apply` node
+    inputs.  See `MetaOp.__call__` for more details.
+
+    Also, make sure to override `Op.out_meta_type` and make it return the
+    expected meta variable type, if it isn't the default: `MetaTensorVariable`.
+    """
     base = tt.Op
 
-    def out_meta_type(self):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.op_sig = inspect.signature(self.obj.make_node)
+
+    def out_meta_type(self, inputs=None):
+        """Return the type of meta variable this `Op` is expected to produce
+        given the inputs.
+
+        The default is `MetaTensorVariable` (corresponding to
+        `TheanoTensorVariable` outputs from the base `Op`).
+        """
         return MetaTensorVariable
 
-    def __call__(self, *args, ttype=None, index=None, name=None):
+    def __call__(self, *args, ttype=None, index=None, **kwargs):
         """Emulate `make_node` for this `Op` and return .
 
-        This will fill-in missing/unreifiable parts of the output variable with
-        logic variables.
+        NOTE: Meta objects will use positional arguments and non-"name" keyword
+        args as `Apply` node inputs.  Also, if some of the `Op` constructor
+        arguments that end up as `Apply` node input arguments are keywords,
+        *use the keywords* and not their positions!
+
+        Otherwise, if a base object can't be referenced, unknown Theano types
+        and index values will be fill-in with logic variables (that can also
+        be specified manually though the keyword arguments `ttype` and `index`).
+
+        Parameters
+        ==========
+        ttype: object (optional)
+            Value to use for an unknown Theano type.  Defaults to a logic
+            variable.
+        index: object (optional)
+            Value to use for an unknown output index value.  Defaults to a
+            logic variable.
         """
-        res_apply = MetaApply(self, args)
-        tt_apply = res_apply.reify()
-        if not self.is_meta(tt_apply):
-            res = MetaVariable.from_obj(tt_apply.default_output())
-            res.name = name
-            return res
-        # TODO: Will this correctly associate the present meta `Op`
-        # and its components with the resulting meta variable?
-        # How about when `tt_apply` is fully reified?
+        name = kwargs.pop('name', None)
 
-        # TODO: Elemwise has an `output_types` method that can be
-        # used to infer the output type of this variable.
-        ttype = ttype or var()
-        index = index if index is not None else var()
+        # Use the `Op`'s default `make_node` arguments, if any.
+        op_arg_bind = self.op_sig.bind(*args, **kwargs)
+        op_arg_bind.apply_defaults()
+        op_args, op_args_unreified = _meta_reify_iter(op_arg_bind.args)
 
-        # We don't have a higher-order meta object model, so being wrong about
-        # the exact type of output variable will cause problems.
-        out_meta_type = self.out_meta_type()
-        res_var = out_meta_type(ttype, tt_apply, index, name)
-        # Allow the base object to be unified, so that reification
-        # can recover the underlying object--instead of recreating
-        # it and sacrificing equality.
-        res_var.obj = var()
+        if not op_args_unreified:
+            tt_out = self.obj(*op_args)
+            res_var = MetaVariable.from_obj(tt_out)
+
+            # If the name is indeterminate, we still want all the reified info,
+            # but we need to make sure that certain parts aren't known.
+            # TODO: In this case, the reified Theano object is a sort of
+            # "proxy" object; we should use this approach for dtype, as well.
+            # TODO: We should also put this kind of logic in the appropriate places
+            # (e.g. `MetaVariable.reify`), when possible.
+            if MetaSymbol.is_meta(name):
+                # This should also invalidate `res_var.obj`.
+                res_var.name = name
+                # Allow the base object to be unified, so that reification
+                # can recover the underlying object--instead of recreating
+                # it and sacrificing equality.
+                res_var.obj = var()
+
+            elif tt_out.name != name:
+                tt_out.name = name
+                res_var.name = name
+
+        else:
+            # XXX: It's not always clear how `Op.make_node` arguments map to
+            # `Apply` node inputs, which is one of the big problem with
+            # Theano's design.  (More generally, it's that `Op`s don't provide
+            # a spec for `Apply` node inputs and outputs at all.)
+
+            # Also, `Apply` inputs can't be `None` (they could be
+            # `tt.none_type_t()`, though).
+            res_apply = MetaApply(
+                self, tuple(filter(lambda x: x, op_arg_bind.args)))
+
+            # TODO: Elemwise has an `output_types` method that can be
+            # used to infer the output type of this variable.
+            ttype = ttype or var()
+            index = index if index is not None else var()
+
+            # XXX: We don't have a higher-order meta object model, so being
+            # wrong about the exact type of output variable will cause
+            # problems.
+            out_meta_type = self.out_meta_type(op_args)
+            res_var = out_meta_type(ttype, res_apply, index, name)
+            res_var.obj = var()
+
         return res_var
 
     def __eq__(self, other):
@@ -307,7 +394,6 @@ class MetaOp(MetaSymbol):
         if not super().__eq__(other):
             return False
 
-        # TODO: Create a property that manages `self.obj`.
         assert self.obj
 
         if self.obj != other.obj:
@@ -322,12 +408,12 @@ class MetaOp(MetaSymbol):
 class MetaElemwise(MetaOp):
     base = tt.Elemwise
 
-    def __call__(self, *args, ttype=None, index=None, name=None):
+    def __call__(self, *args, ttype=None, index=None, **kwargs):
         obj_nout = getattr(self.obj, 'nfunc_spec', None)
         obj_nout = obj_nout[-1] if obj_nout is not None else None
         if obj_nout == 1 and index is None:
             index = 0
-        return super().__call__(*args, ttype=ttype, index=index, name=name)
+        return super().__call__(*args, ttype=ttype, index=index, **kwargs)
 
 
 class MetaDimShuffle(MetaOp):
@@ -339,6 +425,17 @@ class MetaDimShuffle(MetaOp):
         self.input_broadcastable = input_broadcastable
         self.new_order = new_order
         self.inplace = inplace
+
+
+class MetaRandomVariable(MetaOp):
+    base = RandomVariable
+
+    def __init__(self, obj=None):
+        super().__init__(obj=obj)
+        # The `name` keyword parameter isn't an `Apply` node input, so we need
+        # to remove it from the automatically generated signature.
+        self.op_sig = self.op_sig.replace(
+            parameters=list(self.op_sig.parameters.values())[0:4])
 
 
 class MetaApply(MetaSymbol):
@@ -363,6 +460,19 @@ class MetaApply(MetaSymbol):
                     self.obj = tt_var.owner
                     return tt_var.owner
             return self
+
+    @property
+    def nin(self):
+        return len(self.inputs)
+
+    @property
+    def nout(self):
+        if self.outputs is not None:
+            return len(self.outputs)
+        elif self.obj:
+            return len(self.obj.outputs)
+        # TODO: Would be cool if we could return
+        # a logic variable representing this.
 
 
 class MetaVariable(MetaSymbol):
@@ -419,12 +529,21 @@ class MetaVariable(MetaSymbol):
             assert tt_var is not None
             self.obj = tt_var
             return tt_var
+
         return super().reify()
 
 
 class MetaTensorVariable(MetaVariable):
     # TODO: Could extend `theano.tensor.var._tensor_py_operators`, too.
     base = tt.TensorVariable
+
+    @property
+    def ndim(self):
+        if (isinstance(self.type, MetaTensorType) and
+                isinstance(self.type.broadastable, (list, tuple))):
+            return len(self.type.broadcastable)
+        # TODO: Would be cool if we could return
+        # a logic variable representing this.
 
 
 class MetaConstant(MetaVariable):
@@ -535,3 +654,35 @@ class TheanoMetaAccessor(object):
 mt = TheanoMetaAccessor()
 
 mt.dot = MetaSymbol.from_obj(tt.basic._dot)
+
+
+#
+# The wrapped Theano functions will only work when the meta objects
+# are fully reifiable (i.e. can be turned to Theano objects), but it's
+# fairly straight-forward to adjust many of those functions so that they
+# work with meta objects.
+# TODO: Would be nice if we could trick Theano into using meta objects, or
+# a robust use of "proxy" Theano objects
+#
+
+def mt_zeros(shape, dtype=None):
+    if not isinstance(shape,
+                      (list, tuple,
+                       MetaTensorVariable,
+                       tt.TensorVariable)):
+        shape = [shape]
+    if dtype is None:
+        dtype = tt.config.floatX
+    return mt.alloc(np.array(0, dtype=dtype), *shape)
+
+
+mt.zeros = mt_zeros
+
+
+def mt_diag(v, k=0):
+    if v.ndim == 1:
+        return mt.AllocDiag(k)(v)
+    elif v.ndim is not None and v.ndim >= 2:
+        return mt.diagonal(v, offset=k)
+    else:
+        raise ValueError("Input must has v.ndim >= 1.")
