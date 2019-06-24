c53fc535d53ff4e056be9877b1be47923d6865c7
Brandon T. Willard <brandonwillard@gmail.com>
2019-06-24 17:32:38 -0500

    Use fresh logic variables in scale_loc_transform goal
    This allows us to apply `scale_loc_transform` more than once in the same
    miniKanren state (i.e. lvar substitutions).

--- symbolic_pymc/relations/theano/distributions.py
+++ symbolic_pymc/relations/theano/distributions.py
@@ -10,7 +10,7 @@ from unification.utils import transitive_get as walk
 
 from .. import concat
 from ...unify import etuple
-from ...theano.meta import mt, TheanoMetaConstant
+from ...theano.meta import TheanoMetaConstant, mt
 
 from kanren.facts import Relation
 
@@ -84,8 +84,12 @@ def scale_loc_transform(in_expr, out_expr):
 
     TODO: Match larger distribution families and perform transforms from there.
 
+    XXX: PyMC3 rescaling issue (?) doesn't allow us to take the more general
+    approach, which involves separate scale and location rewrites.
+
     """
     # Scale and location transform expression "pattern" for a Normal term.
+    normal_mt = mt.NormalRV(var(), var(), size=var(), rng=var(), name=var())
     n_name_lv = normal_mt.name
     n_mean_lv, n_sd_lv, n_size_lv, n_rng_lv = normal_mt.owner.inputs
     offset_name_mt = var()
@@ -100,6 +104,7 @@ def scale_loc_transform(in_expr, out_expr):
     )
 
     # Scale and location transform expression "pattern" for a Cauchy term.
+    cauchy_mt = mt.CauchyRV(var(), var(), size=var(), rng=var(), name=var())
     c_name_lv = cauchy_mt.name
     c_mean_lv, c_beta_lv, c_size_lv, c_rng_lv = cauchy_mt.owner.inputs
     rct_cauchy_offset_mt = etuple(
@@ -112,6 +117,8 @@ def scale_loc_transform(in_expr, out_expr):
         ),
     )
 
+    # TODO:
+    # uniform_mt = mt.UniformRV(var(), var(), size=var(), rng=var(), name=var())
     # u_name_lv = uniform_mt.name
     # u_a_lv, u_b_lv, u_size_lv, u_rng_lv = uniform_mt.owner.inputs
     # rct_uniform_scale_mt = etuple(
@@ -125,59 +132,24 @@ def scale_loc_transform(in_expr, out_expr):
     #                                          rng=u_rng_lv,
     #                                          name=offset_name_mt))
 
-    # XXX: PyMC3 rescaling issue doesn't allow us to take the more general
-    # approach, which involves separate scale and location rewrites.
-
-    # f(a + X) -> a + f(X)
-    # norm_mean_name_mt = var()
-    # rct_norm_mean_mt = etuple(mt.add, mean_lv,
-    #                           mt.NormalRV(0., sd_lv,
-    #                                       size=norm_size_lv,
-    #                                       rng=norm_rng_lv,
-    #                                       name=norm_mean_name_mt))
-    #
-    # f(a * X) -> a * f(X)
-    # norm_sd_name_mt = var()
-    # rct_norm_sd_mt = etuple(mt.mul, n_sd_lv,
-    #                         mt.NormalRV(n_mean_lv, 1.,
-    #                                     size=n_size_lv,
-    #                                     rng=n_rng_lv,
-    #                                     name=n_name_lv))
-
     rels = (
         conde,
         [
             (eq, in_expr, normal_mt),
-            (
-                conde,
-                [
-                    (constant_neq, n_sd_lv, 1),
-                    (constant_neq, n_mean_lv, 0),
-                    (eq, out_expr, rct_norm_offset_mt),
-                    (concat, [n_name_lv, "_offset"], offset_name_mt),
-                ],
-                # [(constant_neq, mean_lv, 0),
-                #  (eq, out_expr, rct_norm_mean_mt),
-                #  (concat, [norm_name_lv, "_rmean"], norm_mean_name_mt)],
-                # [(constant_neq, sd_lv, 1),
-                #  (eq, out_expr, rct_norm_sd_mt),
-                #  (concat, [norm_name_lv, "_rsd"], norm_sd_name_mt)],
-            ),
+            (constant_neq, n_sd_lv, 1),
+            (constant_neq, n_mean_lv, 0),
+            (eq, out_expr, rct_norm_offset_mt),
+            (concat, [n_name_lv, "_offset"], offset_name_mt),
         ],
         [
             (eq, in_expr, cauchy_mt),
-            (
-                conde,
-                [
-                    (constant_neq, c_beta_lv, 1),
-                    # TODO: Add a positivity constraint for the scale.
-                    (constant_neq, c_mean_lv, 0),
-                    (eq, out_expr, rct_cauchy_offset_mt),
-                    (concat, [c_name_lv, "_offset"], offset_name_mt),
-                ],
-            ),
+            (constant_neq, c_beta_lv, 1),
+            # TODO: Add a positivity constraint for the scale.
+            (constant_neq, c_mean_lv, 0),
+            (eq, out_expr, rct_cauchy_offset_mt),
+            (concat, [c_name_lv, "_offset"], offset_name_mt),
         ],
-        # TODO
+        # TODO:
         # [(eq, in_expr, uniform_mt),
         #  (conde,
         #   [(constant_eq, u_a_lv, 0),
